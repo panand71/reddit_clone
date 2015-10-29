0fcca98bad6103e4186774e70e6489600117a442
Premila Anand <panand71@gmail.com>
2015-10-28 18:38:22 -0700

    adds name and method to validate the urls

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,13 @@ class Link < ActiveRecord::Base
   belongs_to    :user
   has_many      :comments
   acts_as_votable
+  before_validation :smart_add_url_protocol
+
+protected
+
+def smart_add_url_protocol
+  unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+    self.url = "http://#{self.url}"
+  end
+end
 end
